e6379979212fddebd3fde65c27468ccd6f04c93d
Erin Ewart <ems1980@hotmail.com>
2022-04-16 07:56:31 -0400

    fixed functions to save note amd render notes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
+
 const notes = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
@@ -10,48 +13,101 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 function createNewNote(body, notesArray) {
-  const note = body;
-  notesArray.push(note)
+  notesArray = (notes) ? notesArray = notes.notes : notesArray = [];
+  const newNote = body;
+  newNote.id = uuidv4();
+  notesArray.push(newNote);
   fs.writeFileSync(
     path.join(__dirname, './db/db.json'),
     JSON.stringify({ notes: notesArray }, null, 2)
   );
-  return note;
+  return newNote;
 }
 
-function validateNote(note) {
-  if (!note.title || typeof note.title !== 'string') {
+
+function validateNote() {
+  if (!notes.title || typeof notes.title !== 'string') {
     return false;
   }
-  if (!note.text || typeof note.text !== 'string') {
+  if (!notes.text || typeof notes.text !== 'string') {
   return false;
   }
   return true;
-}
+};
 
+//function deleteNote() {
+//   //get id of deleted note
+//   // const deletedNoteId = req.params.id
+//   const noteId  = req.params.id.toString()
+ 
+//   //loop through array of notes
+//   for (let i = 0; i < notesArray.length; i++) {
+//     if( noteId === notes.notes.id) {
+//       const parsedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+//       const newArray = parsedNotes.filter( notes => notes.notes.id.toString() !== deletedNoteId );
+//       fs.writeFileSync('./db/db.json', JSON.stringify(newArray));
+//     }
+//   }
+//   return newArray;
+// }
 
+//   const { id } = req.params;
+//   const notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
 
+//   const noteToDelete = notes.findIndex(note => note.note_id === id);
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-  });
+//   notes.splice(noteToDelete, 1);
 
-app.post('/api/notes', (req, res) => {
-  res.json(req.body);
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateNote(req.body)) {
-    res.status(400).send('The note is not properly formatted.');
-  } else {
-  const note = createNewNote(req.body, notes);
-  res.json(note);
-  }
+//   writeToFile("./db/db.json", notes);
+//   return res.send();
+// };
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes.notes);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+  var newNotes = createNewNote(req.body);
+  validateNote(req.body);
+  res.json(newNotes);
+})
+
+
+app.delete(`/api/notes/:id`, (req, res) => {
+
+  fs.readFile("./db/db.json", "utf8", (err, notes) => {
+    if (err) throw err;
+  const allNotes = JSON.parse(notes)
+  const noteToDelete = req.params.id;
+  console.log (req.params.id)
+  })
+  
+  const newNotesArray = allNotes.filter(note =>
+    notes.notes.id != noteToDelete);
+
+  fs.writeFileSync(__dirname + "/../db/db.json", newNotesArray, (err) => {
+    if (err) throw err;
+  });
+
+  res.json(result);
+});
+
+    
 app.listen(PORT, () => {
 console.log(`API server now on port ${PORT}!`);
 });  
